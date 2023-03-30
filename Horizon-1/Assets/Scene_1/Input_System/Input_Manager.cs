18957d5bc809c3ca0329fb16a2a3d5fc230b1f20
FlyntDenzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-03-30 18:30:28 +0100

    Character
    Imported character to new scene and added an input manager

--- Horizon-1/Assets/Scene_1/Input_System/Input_Manager.cs
+++ Horizon-1/Assets/Scene_1/Input_System/Input_Manager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Input_Manager : MonoBehaviour
 {
   PlayerControls playercontrols;//generating a playercontrols class
-  Vector2 movementInput;
+  public Vector2 movementInput;
 
   void OnEnable(){
     if (playercontrols == null)
     {
-        playercontrols = new PlayerControls();//creating an instance of a playercontrold
+        playercontrols = new PlayerControls();//creating an instance of a playercontrols
+        playercontrols.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();//callback function
     }
+
+    playercontrols.Enable();
+  }
+
+  void OnDisable(){
+    playercontrols.Disable();
   }
 }
