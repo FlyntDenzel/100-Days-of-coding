724c831708ab52dc5de8eeea805a07e360b4c69e
FlyntDenzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-04-02 22:10:33 +0100

    character input addition

--- Project_Alpha/Assets/characterMovement.cs
+++ Project_Alpha/Assets/characterMovement.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class characterMovement : MonoBehaviour
 {
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+
+    PlayerInput input;
+
+    void Awake(){
+        input = new PlayerInput();//creating instance of playerinput class
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void HandleMovement(){
+        //getting parameter values from animator
+        bool isWalking = Input.GetBool(isWalkingHash);
+        bool isRunning = Input.GetBool(isRunningHash);
     }
 }
